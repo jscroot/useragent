0e09fc2bb37f2b81cfe645eca0d6cab9db6354ad
Rolly Maulana Awangga <rolly@awang.ga>
2023-02-23 04:35:03 +0700

    export

--- croot.js
+++ croot.js
@@ -1,4 +1,4 @@
-function isMobile(){
+export function isMobile(){
     let mobile;
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     mobile = true;
